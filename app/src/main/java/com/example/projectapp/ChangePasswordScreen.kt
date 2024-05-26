ffcb3e6bdefcf35ee506f3b1d1e9af773c37b831
Hass <roihas@post.bgu.ac.il>
2024-05-26 11:52:09 +0300

    adding the navigation back to profile screen

--- app/src/main/java/com/example/projectapp/ChangePasswordScreen.kt
+++ app/src/main/java/com/example/projectapp/ChangePasswordScreen.kt
@@ -1,25 +1,40 @@
 package com.example.projectapp
 
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.projectapp.ui.theme.ProjectAppTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+//import kotlin.coroutines.jvm.internal.CompletedContinuation.context
 
 @Composable
 fun ChangePasswordScreen(navController: NavController,modifier: Modifier = Modifier){
@@ -36,11 +51,12 @@ fun ChangePasswordScreen(navController: NavController,modifier: Modifier = Modif
                 )
             ),
         verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
             text = "Change your password",
             modifier = modifier
-                .padding(16.dp),
+                .padding(24.dp),
             style = TextStyle(fontSize = 24.sp),
             color = Color.White
         )
@@ -63,6 +79,34 @@ fun ChangePasswordScreen(navController: NavController,modifier: Modifier = Modif
                 value = ""
 
             )
+            val context = LocalContext.current
+            val coroutineScope = rememberCoroutineScope()
+            Button(
+                onClick = {
+                    Toast.makeText(context, "Password has changed", Toast.LENGTH_SHORT).show()
+                    coroutineScope.launch {
+                        delay(2500)
+                        navController.navigate("ProfileScreen")
+                    }
+                 },
+                modifier = Modifier
+                    .paddingFromBaseline(top = 24.dp,bottom = 8.dp),
+                shape = MaterialTheme.shapes.large,
+                contentPadding = PaddingValues(24.dp),
+                colors = ButtonDefaults.buttonColors(
+                    contentColor = MyColors.ButtonColor,
+                    containerColor = MyColors.ButtonColor
+                )
+            ) {
+                Text(
+                    modifier= Modifier.padding(0.dp),
+                    text= "Submit",
+                    fontSize = 12.sp,
+                    color= Color.White,
+                    textAlign = TextAlign.Center,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
         }
         BottomNavigation(navController = navController)
     }
